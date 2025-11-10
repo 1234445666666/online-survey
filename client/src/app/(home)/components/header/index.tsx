4d44f39340a460007e2a87d13cf1881e0dd4a0e7
1234445666666 <p.k.270607@gmail.com>
2025-11-10 15:34:39 +0300

    fix: styles front and fix adaptive and fix content html

--- client/src/app/(home)/components/header/index.tsx
+++ client/src/app/(home)/components/header/index.tsx
@@ -1,4 +1,5 @@
 import "./header.css";
+import Link from "next/link";
 export default function Header() {
   return (
     <header>
@@ -6,14 +7,13 @@ export default function Header() {
         <nav>
           <div className="logo">SimpleVote</div>
           <div className="nav-links">
-            <a href="#features">Возможности</a>
-            <a href="#how-it-works">Как это работает</a>
-            <a href="#use-cases">Примеры использования</a>
-            <a href="#pricing">Тарифы</a>
+            <Link href="#features">Возможности</Link>
+            <Link href="#how-it-works">Как это работает</Link>
+            <Link href="#use-cases">Примеры использования</Link>
           </div>
           <div className="auth-buttons">
-            <button className="btn btn-outline">Войти</button>
-            <button className="btn btn-primary">Регистрация</button>
+            <button className="btn btn-login">Войти</button>
+            <button className="btn btn-register">Регистрация</button>
           </div>
         </nav>
       </div>
